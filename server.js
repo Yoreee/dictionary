1a1835a360401dde80a09ae0ee9953011dc3d169
Riaz Bacchus <riaz.bacchus@me.com>
2015-12-12 04:11:47 -0500

    set up public and routes folders, middleware, and jade

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 var express = require('express');
+var path = require('path');
+var request = require('request');
+var dotenv = require('dotenv').load();
+var jade = require('jade');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
+
 var app = express();
-var request = require('request')
-var dotenv = require('dotenv').load()
+var routes = require('./routes/index')
+
+
+app.set('views', path.join(__dirname, 'public/views'));
+// app.set('styles', path.join(__dirname, 'public/styles'))
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/', routes);
+
 
 app.listen(3000, function(req, res) {
-	console.log('listening')
-})
+	console.log('listening');
+});
+
+
+
 
 request({
     url: 'https://montanaflynn-dictionary.p.mashape.com/define?word=velleity', //URL to hit
@@ -21,3 +41,6 @@ request({
         console.log(response.statusCode, body);
     }
 });
+
+
+module.exports = app;
