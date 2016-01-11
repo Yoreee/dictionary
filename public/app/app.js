ad2fb26cd9f02eb0c3bd3463a9588e37ed32b680
Riaz Bacchus <riaz.bacchus@me.com>
2016-01-11 01:38:49 -0500

    replacing bootstrap with plain responsive css

--- public/app/app.js
+++ public/app/app.js
@@ -3,8 +3,10 @@
 	var app = angular.module("wordsApp", []);
 
 	app.controller('WordController', function($http) {
+		var that = this;
 		this.query;
-		this.definition;
+		this.data;
+		this.repeatData;
 		this.lookup = function() {
 	        $http({
 		        url: 'https://montanaflynn-dictionary.p.mashape.com/define?word=' + this.query, //URL to hit
@@ -17,6 +19,15 @@
 		    .then(function success(res) {
 		        console.log(res.data)
 		        console.log('success')
+		        that.data = res.data.definitions
+		        that.repeatData = res.data.definitions.map(function(value, index) {
+		        	return {
+		        		data: value,
+		        		value: index +1
+		        	}
+		        })
+		        console.log(that.repeatData);
+		        document.getElementsByClassName('word-input')[0].value = "";
 		    }, function error(res) {
 		        console.log(res.status)
 		    	});
